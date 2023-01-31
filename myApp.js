7bc4372b9dd2c8246d50f80ca818f5284b15b2db
Biniam Tefrie <79765727+beni1218@users.noreply.github.com>
2023-02-01 01:59:43 +0200

    Third commit

--- myApp.js
+++ myApp.js
@@ -2,13 +2,19 @@ let express = require('express');
 let app = express();
 
 //Ex.1
-console.log('Hello World');
+//console.log('Hello World');
 
 //Ex.2
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.send('Hello Express');
-})
+})*/
 
+//Ex.3 Send Static files
+
+app.get('/', (req, res) => {
+    absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+})
 
 
 
